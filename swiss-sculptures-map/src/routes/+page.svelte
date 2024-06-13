3a47de55c4fd382f9571402eea0fb4a174eae670
ZEDz318 <danzzeidan8884@gmail.com>
2024-06-13 12:07:42 +0200

    small change
    changed grid-container (footer) 's z-index

--- swiss-sculptures-map/src/routes/+page.svelte
+++ swiss-sculptures-map/src/routes/+page.svelte
@@ -1,71 +1,62 @@
 <script>
-    import Map from './Map.svelte';
+	import Map from './Map.svelte';
 </script>
 
 <body style="margin: 0; padding: 0;">
+	<Map />
 
-    <Map />
+	<!-- Footer/Legend -->
+	<style>
+		.grid-container {
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			width: 100%;
+			display: grid;
+			grid-template-columns: auto auto;
+			gap: 1rem;
+			padding: 20px;
+			background-color: #d9d9d9;
+			z-index: 100;
+		}
 
-    <!-- Footer/Legend -->
-    <style>
-        .grid-container {
-            position: absolute;
-            bottom: 0; 
-            left: 0; 
-            width: 100%;
-            display: grid;
-            grid-template-columns: auto auto;
-            gap: 1rem; 
-            padding: 20px; 
-            background-color: #D9D9D9;
-        }
+		.col-auto {
+			grid-column: span 1;
+		}
 
-        .col-auto {
-            grid-column: span 1;
-        }
+		h1 {
+			font-weight: bold;
+			color: #333;
+			margin-bottom: 1rem;
+			font-size: 48px;
+		}
 
+		h3 {
+			font-weight: 600;
+			color: #666;
+			margin-bottom: 1rem;
+			font-size: 36px;
+		}
 
-        h1 {
-            font-weight: bold;
-            color: #333;
-            margin-bottom: 1rem;
-            font-size: 48px;
-            
+		h4 {
+			font-size: 28px;
+			font-family: Inter;
+		}
 
-        }
-
-        h3 {
-            font-weight: 600; 
-            color: #666;
-            margin-bottom: 1rem;
-            font-size: 36px;
-        }
-
-        h4{
-            font-size: 28px;
-            font-family: Inter;
-        }
-
-        .l-text {
-            margine-top: 2rem;
-            color: #888; 
-            font-size: 28px;
-        }
-
-
-    </style>
-    <div class="grid-container">
-        <div class='col-auto'>
-            <h1>Schweizer Plastiken</h1>
-            <h3>Klicken Sie auf die Punkte und Linien, um mehr Details zu sehen.</h3>
-        </div>
-        <div>
-            <p class='l-text'>&#128308 ursprünglicher Standort</p>
-            <p class='l-text'>&#128309 neuer Standort</p>
-        </div>
-    </div>
-    
-    
+		.l-text {
+			margine-top: 2rem;
+			color: #888;
+			font-size: 28px;
+		}
+	</style>
+	<div class="grid-container">
+		<div class="col-auto">
+			<h1>Schweizer Plastiken</h1>
+			<h3>Klicken Sie auf die Punkte und Linien, um mehr Details zu sehen.</h3>
+		</div>
+		<div>
+			<p class="l-text">&#128308 ursprünglicher Standort</p>
+			<p class="l-text">&#128309 neuer Standort</p>
+		</div>
+	</div>
 </body>
-
-  

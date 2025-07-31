2c52e3de43a2867a8410407f96ed3d0b324e2bc1
Sebi <sebastian@flick.solutions>
2025-07-31 11:26:23 +0200

    fix slideshow error

--- src/routes/Slideshow.svelte
+++ src/routes/Slideshow.svelte
@@ -20,20 +20,19 @@
 <div class="slideshow-container">
 	{#if images.length === 0}
 		<div class="slide active">
-			<img
-				src="{base}/default-img.png"
-				alt="Sculptures"
-				class="slide-image"
-			/>
+			<img src="{base}/default-img.png" alt="Sculptures" class="slide-image" />
 		</div>
-	{/if}
-	{#each images as image, index}
+	{:else}
+		{#each images as image, index}
 			<div class="slide {index === currentIndex ? 'active' : ''}">
 				<img src={image.image_url} alt="Sculptures" class="slide-image" />
-				<p style="font-size: 7pt; text-align: center;">
+			</div>
+			<div class="slide-text {index === currentIndex ? 'active' : ''}">
+				<p style="font-size: 7pt; text-align: center; position: relative; bottom: 0px;">
 					<span style="font-weight: bold;">Photo:</span>
 					{image.image_credits}
 				</p>
 			</div>
-	{/each}
+		{/each}
+	{/if}
 </div>
